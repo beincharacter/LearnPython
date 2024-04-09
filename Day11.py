ef34c9308c0237eeb637d9e4fb90d75a67245792
beincharacter@gmail.com <shubhampalatwork@gmail.com>
2024-04-09 21:14:27 +0530

    Day 11 code of calculating seconds in a year

---
+++ Day11.py
@@ -0,0 +1,11 @@
+# Define constants for calculation
+seconds_per_minute = 60
+minutes_per_hour = 60
+hours_per_day = 24
+days_per_year = 365
+
+# Calculate seconds in a year
+seconds_per_year = seconds_per_minute * minutes_per_hour * hours_per_day * days_per_year
+
+# Display the result
+print("Seconds in a year:", seconds_per_year)
